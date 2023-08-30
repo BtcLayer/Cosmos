7e869f9b16b12308bfff406c681fd3dad784e7b8
Pragya Srivastava <126488693+SrivastavaPragya@users.noreply.github.com>
2023-08-30 16:20:17 +0530

    Update btc.sol

--- btclayeer/btc.sol
+++ btclayeer/btc.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Importing required libraries and interfaces
-// import "path/to/ILightningNetwork.sol";
-// import "path/to/IZkEVM.sol";
 
 contract BTCLayer {
     // State variables
